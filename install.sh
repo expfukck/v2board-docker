dc75fc238c51deadfd4d1a44f1c48d5753f2c7b0
sudug <60267838+expfukck@users.noreply.github.com>
2022-07-11 23:42:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,13 +80,13 @@ install_base() {
 
     if [[ x"${release}" == x"centos" ]]; then
                  yum install epel-release -y
-                 yum install wget curl unzip tar crontabs socat -y
+                 yum install wget curl unzip tar crontabs socat git -y
 		curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 		curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 		chmod +x /usr/local/bin/docker-compose
     else
                 apt update -y
-                apt install wget curl unzip tar cron socat -y
+                apt install wget curl unzip tar cron socat git -y
                 curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 		curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 		chmod +x /usr/local/bin/docker-compose
