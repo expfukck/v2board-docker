d4010831c35b7b34b21f9e36bc6e80f7f464e5b7
sudug <60267838+expfukck@users.noreply.github.com>
2022-07-11 22:46:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -91,16 +91,7 @@ install_base() {
 		curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 		chmod +x /usr/local/bin/docker-compose
     else
-        systemctl disable firewalld
-        systemctl stop firewalld
-        sudo apt-get update
-		sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-		sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-		systemctl start docker
-		systemctl enable docker
-		curl -fsSL https://get.docker.com | bash -s docker
+                curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 		curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 		chmod +x /usr/local/bin/docker-compose
     fi
